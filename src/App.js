ff15e885dc835077fde41e56dd7f1bf39be000aa
gustavo <bocettodigital@gmail.com>
2021-11-03 22:50:06 -0500

    install read.js and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="text-7xl font-mono font-bold text-center m-80">
+      Hola Mundo!
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color: {
+        //Aquí van los nuevos colores. Ejemplo: nombre: "#00000"
+        //Ejecutar el comando yarn build cada vez que se modifique este archivo
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
