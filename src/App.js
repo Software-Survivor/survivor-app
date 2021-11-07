3de72be8561a69143e104b032c284a6be32af9d2
gustavo <bocettodigital@gmail.com>
2021-11-06 19:43:31 -0500

    App.js modificated whit DarkContext. modeDark implemented on Index.jsx. Add dark.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import Index from './pages/Index';
 import IndexAdmin from './pages/admin/IndexAdmin';
 import Users from './pages/admin/Users';
 import LayoutAdmin from './layouts/LayoutAdmin';
+import { UserContext } from './context/user';
+import { DarkContext } from './context/dark';
+import { useState } from 'react';
 
 
 function App() {
+  const [userData, setUserData] = useState({data: 'testUserData'});
+  const [modeDark, setModeDark] = useState(false);
   return (
+    <DarkContext.Provider value={{modeDark, setModeDark}}>
+    <UserContext.Provider value={{userData, setUserData}}>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Index/>} />
@@ -17,6 +24,8 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+    </UserContext.Provider>
+    </DarkContext.Provider>
   );
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { useUser } from "../context/user";
+import { useDark } from "../context/dark";
 
 const Index = () => {
+  const { userData } = useUser();
+  const { modeDark, setModeDark } = useDark();
+  console.log(userData);
   return (
     <>
-      <div className="text-7xl font-mono font-bold text-center m-80">
-        Hola Mundo!
+      <div>
+        <div className={`${ modeDark ? "text-7xl bg-black font-mono text-white font-bold text-center mt-80": "text-7xl bg-blue-200 font-mono font-bold text-center mt-80" }`} >
+          Hola Mundo!
+          <p className="text-red-300 text-2xl mt-2">
+            useContex: {userData.data}
+          </p>
+        </div>
+        <div className="font-mono text-center mt-2">
+        <button
+          className="bg-gray-300 p-2 rounded border-gray-500 border-solid border-2"
+          onClick={() => setModeDark(!modeDark)}
+        >
+          {modeDark ? "Desactivar" : "Activar"}Dark Mode
+        </button>
+        </div>
       </div>
     </>
   );
