ab9894d1f59955735af5cb74a07b1631db947175
gustavo <bocettodigital@gmail.com>
2021-11-09 18:47:43 -0500

    Sidebar.jsx components ok

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,25 @@ import React from "react";
 const SideBar = () => {
   return (
     <div className="bg-white shadow-lg border-gray-50 rounded-xl w-28 z-10 hidden sm:flex">
-      Sidebar
+      <div className="divide-y divide-gray-100 overflow-scroll text-gray-400 text-4xl">
+        <Items icon="fas fa-columns" items="item"/>
+        <Items icon="fas fa-columns" items="item"/>
+        <Items icon="fas fa-columns" items="item"/>
+        <Items icon="fas fa-columns" items="item"/>
+        <Items icon="fas fa-columns" items="item"/>
+        <Items icon="fas fa-columns" items="item"/>
+      </div>
+    </div>
+  );
+};
+
+const Items = ({icon, items}) => {
+  return (
+    <div className="flex flex-col justify-center items-center h-32 w-26 p-2 hover:text-green-300 ">
+      <a href="#">
+        <i class={icon}></i>
+      </a>
+      <span className="text-xs">{items}</span>
     </div>
   );
 };
