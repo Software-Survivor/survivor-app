abd15100032a01b912b87f33431d804ed9949427
Andrés Felipe Pérez Guizaho <andresfe52@gmail.com>
2021-11-27 18:36:54 -0500

    creción usuarios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/main.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import IndexUsuarios from "./pages/usuarios/Index";
 import IndexAdmin from "./pages/admin/IndexAdmin";
 import Users from "./pages/admin/Users";
 import LayoutAdmin from "./layouts/LayoutAdmin";
@@ -8,31 +9,42 @@ import AdminIndex from "./pages/AdminIndex";
 import { UserContext } from "./context/user";
 import { DarkContext } from "./context/dark";
 import { useState } from "react";
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
+// const httpLink = createHttpLink({
+//   uri: "https://api-proyecta-tic.herokuapp.com/graphql"
+// })
 const httpLink = createHttpLink({
-  
-})
+  uri: "http://localhost:4001/graphql",
+});
 
 const client = new ApolloClient({
-  uri:""
-  cache: new InMemoryCache()
-})
+  uri: "httpLink",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [userData, setUserData] = useState({ data: "testUserData" });
   const [modeDark, setModeDark] = useState(false);
   return (
-    <ApolloProvider client ={}>
+    <ApolloProvider client={client}>
       <DarkContext.Provider value={{ modeDark, setModeDark }}>
         <UserContext.Provider value={{ userData, setUserData }}>
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Index />} />
+              <Route path="/" element={<LayoutAdmin />} >
+                <Route path="" element={<Index />} />
+                <Route path="/usuarios" element={<IndexUsuarios />} />
+              </Route>
               <Route path="/admin/index" element={<AdminIndex />} />
               <Route path="/admin" element={<LayoutAdmin />}>
                 <Route path="" element={<IndexAdmin />} />
-                <Route path="usuarios" element={<Users />} />
+                <Route path="users" element={<Users />} />
               </Route>
             </Routes>
           </BrowserRouter>
---
+++ src/pages/usuarios/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexUsuarios = () => {
+  return (
+      <div>
+          <h1>TOdos los usuarios</h1>
+      </div>
+  )
+}
+
+export default IndexUsuarios
