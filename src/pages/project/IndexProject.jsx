fb1c4bad3ca024596c406c3c0fefff18a455f306
gustavo <bocettodigital@gmail.com>
2021-12-13 21:24:04 -0500

    mejorar en el diseo del modulo de proyectos

--- src/pages/project/IndexProject.jsx
+++ src/pages/project/IndexProject.jsx
@@ -186,61 +186,6 @@ const IndexProject = () => {
 
   if (loading) return <div>Cargando....</div>;
 
-<<<<<<< HEAD
-=======
-  const circeIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="8"
-      height="8"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-circle"
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-    </svg>
-  );
-  const sx = "15";
-  const edit = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={sx}
-      height={sx}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-edit"
-    >
-      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-    </svg>
-  );
-  const plus = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={sx}
-      height={sx}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-plus-circle"
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-      <line x1="12" y1="8" x2="12" y2="16"></line>
-      <line x1="8" y1="12" x2="16" y2="12"></line>
-    </svg>
-  );
->>>>>>> fcc2c704feca20d368b0149ac90d3936e50c423b
   return (
     <>
       <div className="flex flex-col px-4 mt-4 ">
